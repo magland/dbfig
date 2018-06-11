20bc76ab96940bb639104fc165a5c612a15d3c30
crman1 <cle@math.upenn.edu>
2018-06-11 16:46:15 -0400

    Add files via upload
    I allow the user to specify a directory of the path/.dbfig for the database to be stored in.

--- dbfig_storage_path.m
+++ dbfig_storage_path.m
@@ -1,6 +1,18 @@
-function path=dbfig_storage_path()
-path='/tmp/dbfig_storage';
-if ~exist(path,'dir')
-    mkdir(path)
-end;
+function dpath=dbfig_storage_path(dbpath)
+if nargin == 1
+    dpath=dbpath;
+    return
+end
+DPATH=getenv('DBFIG_HOME_DIR');
+if ~(size(DPATH) == size([]))
+    dpath=[DPATH '/.dbfig'];
+    if ~exist(dpath,'dir')
+        mkdir(dpath);
+    end;
+else
+    dpath='$home/.dbfig';
+    if ~exist(dpath,'dir')
+        mkdir(dpath);
+    end;
+end
 end
